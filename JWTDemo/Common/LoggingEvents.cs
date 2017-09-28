8f37bcfcd9725a0edbe37596b9ad481dccf8b3f6
irisshing1211 <irisshing1211@gmail.com>
2017-09-28 11:04:23 +0800

    add log in token

--- JWTDemo/Common/LoggingEvents.cs
+++ JWTDemo/Common/LoggingEvents.cs
@@ -7,6 +7,11 @@ namespace JWTDemo
 {
     public class LoggingEvents
     {
+        public const int Timeout = 408;
+        public const int BadRequest = 400;
+        public const int Unauthorized = 401;
+
+
         public const int Location = 0000;
         public const int Login = 1000;
     }
